59d41e8fe29e0484905cbf8b704efb87fbb8a265
Šoral <131005143+ondraska@users.noreply.github.com>
2025-02-04 15:09:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+print("hello world", 15 ) # pokud chci výce datových typů musím si je oddělit čárkou
+
+# Vytvoření proměné (deklarace/inicializace)
+cislo = 1 # Do proměné cislo je uložená hodnota 1
+
+print("Druhý způsob s proměnou", cislo)
+
+# Vytvoření proměnné s názvem text
+text = "Zde je v proměné uložený text" # Datový typ string
+
+# Do konzole vypisujeme obě proměné, odělujeme je čárkou
+print(text, cislo)
+
+# text - proměnná text vypíše co je v ní uložené
+# cislo - proměná vypíše číslo, který je v proměnné uložena
+
+# vytvoření vstupní hodnoty - Uživatel musí zadat hodnotu do terminálu
+# následně se hodnota zadaná do terminálu uloží do proměnné
+vstupni_promnenna = input() # inpur() - příkaz pro vstupní data
+
+# input() - do závorek zapisujeme případnou zprávu pro uživatele, který se vypíše v terminálu
+
+druha_vstupni_promenna =input("zadejte číslo: ")
+
+# print() print nám vypíše do konzole, co uživatel zadal do inputu
+print(vstupni_promnenna, druha_vstupni_promenna )
+ 
