3ec9e2ae01368912fbb73b20678857eec6bb714a
Šoral <131005143+ondraska@users.noreply.github.com>
2025-02-11 15:20:55 +0100

    Add files via upload

---
+++ výpis.py
@@ -0,0 +1,17 @@
+jmeno = "Ondřej"
+
+prijmeni = "Šoral"
+
+vek = 16
+
+vyska = 1.75
+
+pohlavi = 0
+
+print(jmeno, prijmeni)
+
+print("Je mu", vek, "let")
+
+print("měří", vyska, "metrů")
+
+print("pohlavi", pohlavi)
