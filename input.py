35e3dee526cb2dfadbe0758a9d47ed9bf560da0b
Šoral <131005143+ondraska@users.noreply.github.com>
2025-02-18 15:02:52 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,41 @@
+sklenice_1 = "mlíko"
+sklenice_2 = "pivo"
+herdyn = ""
+
+print(f"Aktuální stav vypadá takto:")
+print("sklenice_1 = mlíko")
+print("sklenice_2 = pivo")
+print("herdyn = ")
+
+print(f"{sklenice_1} ze sklenice 1 přeliju do herdyna ")
+
+sklenice_1 = ""
+sklenice_2 = "pivo"
+herdyn = "mlíko"
+
+print(f"Aktuální stav vypadá takto:")
+print("sklenice 1 =")
+print("sklenice 2 = pivo")
+print("herdyn = mlíko")
+
+print(f"{sklenice_2} ze sklenice 2 přeliju do sklenice 1 ")
+
+sklenice_1 = "pivo"
+sklenice_2 = ""
+herdyn = "mlíko"
+
+print(f"Aktuální stav vypadá takto:")
+print("sklenice 1 = pivo")
+print("sklenice 2 =")
+print("herdyn = mlíko")
+
+print(f"{herdyn} z herdyna přeliju do sklenice 2 ")
+
+sklenice_1 = "pivo"
+sklenice_2 = "mlíko"
+herdyn = ""
+
+print(f"Aktuální stav vypadá takto:")
+print("sklenice 1 = pivo")
+print("sklenice 2 = mlíko")
+print("herdyn = ")
